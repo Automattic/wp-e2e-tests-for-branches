0af024f2436d4e6bd326446572370a6c516b729d
Alister Scott <alister.scott@gmail.com>
2018-07-06 11:43:53 +1000

    Add more live branch output for debugging calypso.live errors (#34)

--- wait-for-running-branch.sh
+++ wait-for-running-branch.sh
@@ -8,7 +8,16 @@ fi
 COUNT=0
 RESETCOUNT=60 # 5sec retry = Reset the branch after 5 minutes
 MAXCOUNT=120  # 5sec retry = Cancel after 10 minutes
+
 STATUS=$(curl https://calypso.live/status?branch=$BRANCHNAME 2>/dev/null)
+
+#Curl to start with no matter what
+echo "Branch status = $STATUS, running curl https://calypso.live/?branch=$BRANCHNAME"
+curl https://calypso.live/?branch=$BRANCHNAME >/dev/null 2>&1
+
+STATUS=$(curl https://calypso.live/status?branch=$BRANCHNAME 2>/dev/null)
+
+echo "Branch status after initial curl = $STATUS https://calypso.live/status?branch=$BRANCHNAME"
 
 until $(echo $STATUS | grep -wqe "Ready\|NeedsPriming" ); do
   if [ $COUNT == $MAXCOUNT ]; then
@@ -28,4 +37,5 @@ until $(echo $STATUS | grep -wqe "Ready\|NeedsPriming" ); do
   sleep 5
   STATUS=$(curl https://calypso.live/status?branch=$BRANCHNAME 2>/dev/null)
   ((COUNT++))
+  echo "Branch status now = $STATUS https://calypso.live/status?branch=$BRANCHNAME"
 done
