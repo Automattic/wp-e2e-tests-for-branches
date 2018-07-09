21675a5cf7e64729897e02cfa48c499154b6600e
Brent Sessions <brent.sessions@automattic.com>
2018-07-09 11:45:09 -0600

    Add a check to make sure the site is actually loaded and not showing dserve

--- wait-for-running-branch.sh
+++ wait-for-running-branch.sh
@@ -8,6 +8,7 @@ fi
 COUNT=0
 RESETCOUNT=60 # 5sec retry = Reset the branch after 5 minutes
 MAXCOUNT=120  # 5sec retry = Cancel after 10 minutes
+SITEWAITCOUNT=30 # 5sec retry = Stop waiting after 2.5 minutes
 
 STATUS=$(curl https://calypso.live/status?branch=$BRANCHNAME 2>/dev/null)
 
@@ -38,4 +39,20 @@ until $(echo $STATUS | grep -wqe "Ready\|NeedsPriming" ); do
   STATUS=$(curl https://calypso.live/status?branch=$BRANCHNAME 2>/dev/null)
   ((COUNT++))
   echo "Branch status now = $STATUS https://calypso.live/status?branch=$BRANCHNAME"
+done
+
+SITE=$(curl https://calypso.live/?branch=$BRANCHNAME 2>/dev/null)
+COUNT=0
+until ! $(echo $SITE | grep -q "dserve" ); do
+  if [ $COUNT == $SITEWAITCOUNT ]; then
+    echo "Reached maximum allowed wait time, quitting"
+    exit 1
+  fi
+
+  echo "Branch status is $STATUS, but site is not up yet. Waiting until it is up."
+
+  ((COUNT++))
+  sleep 5
+  SITE=$(curl https://calypso.live/?branch=$BRANCHNAME 2>/dev/null)
+  STATUS=$(curl https://calypso.live/status?branch=$BRANCHNAME 2>/dev/null)
 done
