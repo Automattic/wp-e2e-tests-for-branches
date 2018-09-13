00efd7fc00c87e42bb688e10036a0278dd44b6fa
Brent Sessions <31110506+bsessions85@users.noreply.github.com>
2018-09-13 08:54:30 -0600

    update to use subdomains (#42)
    * update to use subdomains
    
    * fixing url

--- wait-for-running-branch.sh
+++ wait-for-running-branch.sh
@@ -10,15 +10,15 @@ RESETCOUNT=60 # 5sec retry = Reset the branch after 5 minutes
 MAXCOUNT=120  # 5sec retry = Cancel after 10 minutes
 SITEWAITCOUNT=30 # 5sec retry = Stop waiting after 2.5 minutes
 
-STATUS=$(curl https://calypso.live/status?hash=$sha 2>/dev/null)
+STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
 
 #Curl to start with no matter what
-echo "Branch status = $STATUS, running curl https://calypso.live/?hash=$sha"
-curl https://calypso.live/?hash=$sha >/dev/null 2>&1
+echo "Branch status = $STATUS, running curl https://hash-$sha.calypso.live/"
+curl https://hash-$sha.calypso.live/ >/dev/null 2>&1
 
-STATUS=$(curl https://calypso.live/status?hash=$sha 2>/dev/null)
+STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
 
-echo "Branch status after initial curl = $STATUS https://calypso.live/status?hash=$sha"
+echo "Branch status after initial curl = $STATUS https://hash-$sha.calypso.live/status"
 
 until $(echo $STATUS | grep -wqe "Ready\|NeedsPriming" ); do
   if [ $COUNT == $MAXCOUNT ]; then
@@ -26,22 +26,22 @@ until $(echo $STATUS | grep -wqe "Ready\|NeedsPriming" ); do
     exit 1
   elif [ $COUNT == $RESETCOUNT ]; then
     echo "Reached reset timeout, attempting to reset the branch"
-    curl https://calypso.live/?hash=$sha\&reset=true >/dev/null 2>&1
+    curl https://hash-$sha.calypso.live/?reset=true >/dev/null 2>&1
   fi
 
   # If it's still showing NotBuilt, then curl the branch directly rather than the status endpoint
   if [ "NotBuilt" == "$STATUS" ]; then
-    echo "Branch status = $STATUS, running curl https://calypso.live/?hash=$sha"
-    curl https://calypso.live/?hash=$sha >/dev/null 2>&1
+    echo "Branch status = $STATUS, running curl https://hash-$sha.calypso.live/"
+    curl https://hash-$sha.calypso.live/ >/dev/null 2>&1
   fi
 
   sleep 5
-  STATUS=$(curl https://calypso.live/status?hash=$sha 2>/dev/null)
+  STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
   ((COUNT++))
-  echo "Branch status now = $STATUS https://calypso.live/status?hash=$sha"
+  echo "Branch status now = $STATUS https://hash-$sha.calypso.live/status"
 done
 
-SITE=$(curl https://calypso.live/?hash=$sha 2>/dev/null)
+SITE=$(curl https://hash-$sha.calypso.live/ 2>/dev/null)
 COUNT=0
 until ! $(echo $SITE | grep -q "DServe Calypso" ); do
   if [ $COUNT == $SITEWAITCOUNT ]; then
@@ -53,6 +53,6 @@ until ! $(echo $SITE | grep -q "DServe Calypso" ); do
 
   ((COUNT++))
   sleep 5
-  SITE=$(curl https://calypso.live/?hash=$sha 2>/dev/null)
-  STATUS=$(curl https://calypso.live/status?hash=$sha 2>/dev/null)
+  SITE=$(curl https://hash-$sha.calypso.live/ 2>/dev/null)
+  STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
 done
